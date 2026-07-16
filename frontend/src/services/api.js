1b92f6038e76ff4a492b91e5d2d3b5b1d7378738
Kaylene06 <mkaylene06@gmail.com>
2026-07-16 12:17:25 +0200

    connect render to vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://deskflow-ticket-service-request-portal-1.onrender.com",
 });
 
 // Attach token automatically
